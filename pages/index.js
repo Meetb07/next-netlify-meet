66de699521c0cf5897a72a74d5a791ecac11b2be
Meetb07 <125444679+Meetb07@users.noreply.github.com>
2023-02-15 14:11:55 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,48 @@
-import random
+// Function to authenticate the user with OTP
+function authenticateUser() {
+  // Assume this function sends an OTP to the user's phone number and waits for the user to enter it
+  // Once the user enters the OTP, it returns true if the OTP is valid, false otherwise
+  return true;
+}
 
-# User authentication function
-def authenticate_user():
-    # Assume this function sends an OTP to the user's phone number and waits for the user to enter it
-    # Once the user enters the OTP, it returns True if the OTP is valid, False otherwise
-    return True
+// Programming-related Q&A
+function answerQuestion(question) {
+  // Assume this function takes in a question as input and returns a random answer
+  const answers = [
+    "It depends on the language you're using. Could you provide more information?",
+    "Yes, that's possible. Here's how you can do it...",
+    "I'm not sure. Could you please provide more context?",
+    "No, that's not possible. Here's why...",
+    "That's a common mistake. Here's how to fix it...",
+    "I'm sorry, I don't know the answer to that.",
+    "That's a great question! Here's what you need to know...",
+  ];
+  return answers[Math.floor(Math.random() * answers.length)];
+}
 
-# Programming-related Q&A
-def answer_question(question):
-    # Assume this function takes in a question as input and returns a random answer
-    answers = [
-        "It depends on the language you're using. Could you provide more information?",
-        "Yes, that's possible. Here's how you can do it...",
-        "I'm not sure. Could you please provide more context?",
-        "No, that's not possible. Here's why...",
-        "That's a common mistake. Here's how to fix it...",
-        "I'm sorry, I don't know the answer to that.",
-        "That's a great question! Here's what you need to know...",
-    ]
-    return random.choice(answers)
+// Function to start the chatbot
+function startChatbot() {
+  // Authenticate the user
+  const authenticated = authenticateUser();
+  if (!authenticated) {
+    console.log("Sorry, we couldn't authenticate you. Please try again later.");
+    return;
+  }
 
-# Chatbot main function
-def chatbot():
-    # Authenticate the user
-    authenticated = authenticate_user()
-    if not authenticated:
-        print("Sorry, we couldn't authenticate you. Please try again later.")
-        return
-    
-    # Once the user is authenticated, start the chatbot
-    print("Hi there! I'm the CodeCommunity chatbot. How can I help you today?")
-    while True:
-        # Get user input
-        question = input("> ")
-        
-        # Check if the user wants to exit
-        if question.lower() in ['exit', 'bye', 'quit']:
-            print("Goodbye!")
-            break
-        
-        # Answer the user's question
-        answer = answer_question(question)
-        print(answer)
+  // Once the user is authenticated, start the chatbot
+  console.log("Hi there! I'm the CodeCommunity chatbot. How can I help you today?");
+  let question = prompt("Enter your question:");
+  while (question && question.toLowerCase() !== "exit") {
+    // Answer the user's question
+    const answer = answerQuestion(question);
+    console.log(answer);
+
+    // Ask the user for another question
+    question = prompt("Enter your question:");
+  }
+
+  console.log("Goodbye!");
+}
+
+// Call the startChatbot function to begin the chatbot session
+startChatbot();
